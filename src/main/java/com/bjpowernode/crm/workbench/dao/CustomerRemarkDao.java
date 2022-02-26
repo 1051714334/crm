655ccdc1b7ba6fc511321c2c7b69724319bb7ab0
Administrator <https://github.com/1051714334/crm.githttps://github.com/1051714334/crm.githttps://github.com/1051714334/crm.git>
2022-02-27 01:39:20 +0800

    v6.1 customerOver

--- src/main/java/com/bjpowernode/crm/workbench/dao/CustomerRemarkDao.java
+++ src/main/java/com/bjpowernode/crm/workbench/dao/CustomerRemarkDao.java
@@ -2,7 +2,17 @@ package com.bjpowernode.crm.workbench.dao;
 
 import com.bjpowernode.crm.workbench.domain.CustomerRemark;
 
+import java.util.List;
+
 public interface CustomerRemarkDao {
 
     int save(CustomerRemark customerRemark);
+
+    List<CustomerRemark> showRemarkList(String customerId);
+
+    int saveRemark(CustomerRemark cusRemark);
+
+    int deleteRemark(String id);
+
+    int updateRemark(CustomerRemark cr);
 }
